0274482dd897a290c8c9d6678408a53369583c7a
Josh Donenberg <joshdonenberg@Joshs-MavenWaveBook-Pro.local>
2022-06-15 14:33:31 -0500

    yes

--- network.tf
+++ network.tf
@@ -1,10 +1,8 @@
-# module "vpc" {
-#   source                                 = "terraform-google-modules/network/google//modules/vpc"
-#   project_id                             = "basic-app-353415"
-#   network_name                           = "tform-project"
-#   routing_mode                           = "GLOBAL"
-#   shared_vpc_host                        = false
-#   delete_default_internet_gateway_routes = false
-# }
-
-output "test"
+ module "vpc" {
+   source                                 = "terraform-google-modules/network/google//modules/vpc"
+   project_id                             = "basic-app-353415"
+   network_name                           = "tform-project"
+   routing_mode                           = "GLOBAL"
+   shared_vpc_host                        = false
+   delete_default_internet_gateway_routes = false
+ }
